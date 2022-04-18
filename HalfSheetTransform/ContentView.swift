30d7f9e6a4a0e81565c6b63cd9b0a52bc43e040b
Philip Davis <philipcdavis11@gmail.com>
2022-04-18 08:09:00 -0700

    Initial Commit

---
+++ HalfSheetTransform/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HalfSheetTransform
+//
+//  Created by Philip Davis on 4/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HalfSheetTransform/HalfSheetTransformApp.swift
@@ -0,0 +1,17 @@
+//
+//  HalfSheetTransformApp.swift
+//  HalfSheetTransform
+//
+//  Created by Philip Davis on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct HalfSheetTransformApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
